4d1da2e5823546cc2f30179956d689308a3ebbbe
Cody Wittmaack <cwittmaack@hotmail.com>
2022-10-19 16:07:14 -0500

    Updates made

---
+++ public/main.js
@@ -0,0 +1,35 @@
+let price1 = document.querySelector('.trip1price')
+
+const uBroke = evt => {
+    alert("You're too broke for this fool!")
+}
+
+price1.addEventListener('click', uBroke)
+
+
+
+let price2 = document.querySelector('.trip2price')
+
+const uWin = evt => {
+    alert("You won a free trip!  Email me us your soical.")
+}
+
+price2.addEventListener('click', uWin)
+
+
+let price3 = document.querySelector('.trip3price')
+
+const uLose = evt => {
+    alert("No kids allowed!")
+}
+
+price3.addEventListener('click', uLose)
+
+
+let price4 = document.querySelector('.trip4price')
+
+const yo = evt => {
+    alert("No peeing in the pool!")
+}
+
+price4.addEventListener('click', yo)
