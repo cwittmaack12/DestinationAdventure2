45a119cc07171887c43d6486314713049e1986b8
Cody Wittmaack <cwittmaack@hotmail.com>
2022-10-20 14:08:08 -0500

    added rollbar

--- server/server.js
+++ server/server.js
@@ -3,6 +3,17 @@ const cors = require('cors')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '7a4918c9e9b74076b9293caa1c64838b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static ('public'))
